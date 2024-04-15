0a2fa19b4b4736ddb875f445d0ff95c9791501b6
Shrinivas-S-S <shrinivas.s@newstreettech.com>
2024-04-15 12:54:05 +0530

    package completed

---
+++ Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.css";
+const Button = () => {
+  const style = {};
+  return (
+    <div class="buttons-container">
+      <button class="button-arounder">Hover Me</button>
+    </div>
+  );
+};
+
+export default Button;
